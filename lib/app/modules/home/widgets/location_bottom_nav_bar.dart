ff95c9d4ab93caea950c1da57882c534131801c4
ronit idea usher <ronitrameja28@gmail.com>
2024-09-10 13:15:23 +0530

    added ci/cd

--- lib/app/modules/home/widgets/location_bottom_nav_bar.dart
+++ lib/app/modules/home/widgets/location_bottom_nav_bar.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:master_v_p_n/app/services/responsive_size.dart';
 
 class LocationSelectionBottomNavBar extends StatelessWidget {
   const LocationSelectionBottomNavBar({super.key});
@@ -12,7 +14,42 @@ class LocationSelectionBottomNavBar extends StatelessWidget {
           onTap: () {
             // Navigate to the location selection screen
           },
-          child: Padding(padding: EdgeInsetsGeometry),
+          child: DecoratedBox(
+            decoration: const BoxDecoration(color: Colors.redAccent),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4.1.w),
+              child: SizedBox(
+                height: 62.kh,
+                child: Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.flag_circle,
+                      color: Colors.white,
+                      size: 36.kh,
+                    ),
+                    12.kwidthBox,
+                    Text(
+                      "Select Country / Location",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.kh,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const Spacer(),
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.redAccent,
+                        size: 26.kh,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
